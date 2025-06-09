24496d83bbefd6ee30c076073b074482cfd8e3a9
camilacalle <141865307+camilacalle@users.noreply.github.com>
2025-06-09 08:27:25 -0300

    Create funcionesVenta.h

---
+++ funcionesVenta.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_VENTA_H
+#define FUNCIONES_VENTA_H
+
+#include "Venta.h"
+#include "Lista.h"
+
+void agregarVenta(Lista<Venta>& ventas);
+void eliminarVenta(Lista<Venta>& ventas);
+void modificarVenta(Lista<Venta>& ventas);
