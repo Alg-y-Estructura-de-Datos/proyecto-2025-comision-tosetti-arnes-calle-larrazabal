88a53c54851cf5b5b7002b5cc58c2505d4ddc908
camilacalle <141865307+camilacalle@users.noreply.github.com>
2025-06-09 10:16:06 -0300

    consultaDinmica.h

---
+++ consultaDinmica.h
@@ -0,0 +1,10 @@
+#ifndef CONSULTASDINAMICAS_H
+#define CONSULTASDINAMICAS_H
+
+#include "Lista.h"
+#include "Venta.h"
+
+// Declarás solo lo que se llama desde main
+void menuConsultasDinamicas(const Lista<Venta>& ventas);
+
+#endif
